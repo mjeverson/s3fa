c23b878c57f34932b3f3f529c7b0376a98d0587e
mjeverson <everson.mike@gmail.com>
2016-07-09 11:06:10 -0400

    Removes test node files, fixes dir structure

--- server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-connect().use(serveStatic('app')).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
